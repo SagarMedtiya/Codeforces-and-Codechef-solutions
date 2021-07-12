021d6c7e5c8eb0a429055dd9ef136fb79eb43fa2
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2021-07-12 21:34:37 +0530

    Add files via upload

---
+++ 4A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a;
+	cin>>a;
+	if(a==2){
+		cout<<"No";
+	}
+	else if(a%2 ==0){
+		cout<<"Yes";
+	}
+	else{
+		cout<<"No";
+	}
+	return 0;
+}
